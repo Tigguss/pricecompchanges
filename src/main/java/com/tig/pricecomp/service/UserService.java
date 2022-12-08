b0bc719693f18045d9d0153d829b6e6f6154c0e1
Tigguss <103384592+Tigguss@users.noreply.github.com>
2022-12-08 01:00:25 +0000

    capREM

--- src/main/java/com/tig/pricecomp/service/UserService.java
+++ src/main/java/com/tig/pricecomp/service/UserService.java
@@ -54,9 +54,7 @@ public class UserService implements IUserService {
     @Autowired
     private Environment env;
 
-    public static final String TOKEN_INVALID = "invalidToken";
-    public static final String TOKEN_EXPIRED = "expired";
-    public static final String TOKEN_VALID = "valid";
+
 
     public static String QR_PREFIX = "https://chart.googleapis.com/chart?chs=200x200&chld=M%%7C0&cht=qr&chl=";
     public static String APP_NAME = "SpringRegistration";
